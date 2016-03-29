970272da14bc155a63b595d25f6ff3d5e9daf33d
Keshav Parwal <keshav@idforweb.gtisc.gatech.edu>
2016-03-29 00:51:22 +0000

    skeleton added to server

--- server.js
+++ server.js
@@ -3,8 +3,45 @@
 var http = require('http');
 var querystring = require('querystring');
 var merkle_base = require('merkle-tree');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type':'text/plain'});
-	response.end("Hello, I'm the server!");
-}).listen(8080);
+//Connect to mongodb
+
+//Create merkle tree
+
+//Message created
+
+app.post('/', function (request, response) {
+	console.log(request);
+	response.end('Received POST request!');
+});
+
+//Commit merkle tree to mongodb, write hash to blockchain, store txid with merkle root
+var interval = setInterval(function() {
+	//save tree to mongodb and get root
+
+	//write hash to blockchain
+	
+	//retrieve txid and store with merkle root
+},1000*60*60*24);
+
+//complete message, commit to merkle tree
+app.post('/complete', function (request, response) {
+	//find message in buffer list
+
+	//complete message with new information
+	
+	//write tree to mongo
+});
+
+//Is my information in this merkle tree?
+app.get('/verify', function (request, response) {
+	//grab tree from block_num
+	
+	//search tree for information
+	
+	//return success or failure
+});
+
+app.listen(8080);
