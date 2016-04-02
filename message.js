60be7293dea2d21321bd271e55b1747fce994ba9
kparwal <kparwal260@gmail.com>
2016-04-02 15:17:34 +0000

    comments and updated readme, server skeleton

---
+++ message.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var message = function (){};
+message.prototype.complete = function () {
+}
+
+exports.message = message;
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 'use strict'
-
 var http = require('http');
 var querystring = require('querystring');
 var merkle_base = require('merkle-tree');
 var express = require('express');
+var mongo_client = require('mongodb');
+var python_shell = require('python-shell');
+var assert = require('assert');
 var app = express();
-
+var block_num = 0; //persistence required across runs
+var buffer = [];
 //Connect to mongodb
 
+var url = 'mongodb://localhost:27017/test';
+mongo_client.connect(url, function(err, db) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server.");
+	db.close();
+});
+
 //Create merkle tree
+var config = new merkle_base.Config({N:4, M:16});
+var current_tree = new merkle_base.MemTree(config);
 
 //Message created
-
 app.post('/', function (request, response) {
 	console.log(request);
 	response.end('Received POST request!');
@@ -20,7 +31,7 @@ app.post('/', function (request, response) {
 //Commit merkle tree to mongodb, write hash to blockchain, store txid with merkle root
 var interval = setInterval(function() {
 	//save tree to mongodb and get root
-
+	
 	//write hash to blockchain
 	
 	//retrieve txid and store with merkle root
