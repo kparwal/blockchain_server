6255fa19bb7527f2a8cf30fc38465ab73ef963b7
kparwal <kparwal260@gmail.com>
2016-04-02 16:51:54 +0000

    filled out message.js

--- message.js
+++ message.js
@@ -1,7 +1,20 @@
 'use strict'
 
-var message = function (){};
-message.prototype.complete = function () {
-}
+var message = function (userid, fname, lname, hash){
+	this.userid = userid;
+	this.fname = fname;
+	this.lname = lname;
+	this.key_hash = hash;
+	this.timestamps = [];
+};
+
+message.prototype.timestamp = function(timestamp) {
+	this.timestamps.push(timestamp);
+};
+
+message.prototype.complete = function (timestamp) {
+	this.pub_key = key;
+	this.vid_url = url;
+};
 
 exports.message = message;
